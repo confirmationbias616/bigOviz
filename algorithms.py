f2cffe743f6f660f31618b1772163baa6d6d49ae
confirmationbias616 <33437097+confirmationbias616@users.noreply.github.com>
2018-03-21 04:57:31 -0400

    Improvements to code clarity
    Added Argparse "help" descriptions, Cleaned up code to conform to PEP8 and misc other improvements

--- algorithms.py
+++ algorithms.py
@@ -1,87 +1,41 @@
-def merge_lists_scan(list1, list2):
-    merged_list = []
-    a = 0  # current index for list1
-    b = 0  # current index for list2
 
-    while a < len(list1) or b < len(list2):
-        if b >= len(list2):
-            while a < len(list1):
-                merged_list.append(list1[a])
-                a += 1
-        elif a >= len(list1):
-            while b < len(list2):
-                merged_list.append(list2[b])
-                b += 1
-        elif list1[a] <= list2[b]:
-            merged_list.append(list1[a])
-            a += 1
-        elif list1[a] > list2[b]:
-            merged_list.append(list2[b])
-            b += 1
-
-    return merged_list
-
-'''
-def merge_lists_pop(list1, list2):
-    merged_list = []
-    listA = list1.copy()
-    listB = list2.copy()
-
-    while len(listA) > 0 or len(listB) > 0:
-        if len(listB) == 0:
-            while len(listA) > 0:
-                merged_list.append(listA.pop(0))
-        elif len(listA) == 0:
-            while len(listB) > 0:
-                merged_list.append(listB.pop(0))
-        elif listA[0] <= listB[0]:
-            merged_list.append(listA.pop(0))
-        elif listA[0] > listB[0]:
-            merged_list.append(listB.pop(0))
-
-    return merged_list
-'''
-
-def merge_lists_py(list1, list2):
-    merged_list = sorted(list1 + list2)
-
-    return merged_list
+def itertools_algo10(list1):
+    import itertools
+    return [seq for i in range(len(list1), 0, -1) for
+            seq in itertools.combinations(list1, i) if sum(seq) == 10]
 
 
-def merge_lists_cake(my_list, alices_list):
+def itertools_algo1000(list1):
+    import itertools
+    return [seq for i in range(len(list1), 0, -1) for
+            seq in itertools.combinations(list1, i) if sum(seq) == 1000]
 
-    # set up our merged_list
-    merged_list_size = len(my_list) + len(alices_list)
-    merged_list = [None] * merged_list_size
 
-    current_index_alices = 0
-    current_index_mine = 0
-    current_index_merged = 0
+def subset_sum10(list1):
+    target = 10
+    partial = []
 
-    while current_index_merged < merged_list_size:
+    def subset_sum(list1, target, partial=[]):
+        s = sum(partial)
+        if s >= target:
+            return  # if we reach the number why bother to continue
 
-        is_my_list_exhausted = current_index_mine >= len(my_list)
-        is_alices_list_exhausted = current_index_alices >= len(alices_list)
+    for i in range(len(list1)):
+            n = list1[i]
+            remaining = list1[i + 1:]
+            subset_sum(remaining, target, partial + [n])
 
-        # case: next comes from my list
-        # my list must not be exhausted, and EITHER:
-        # 1) Alice's list IS exhausted, or
-        # 2) the current element in my list is less
-        #    than the current element in Alice's list
-        if not is_my_list_exhausted and \
-            (is_alices_list_exhausted or
-             (my_list[current_index_mine] <
-              alices_list[current_index_alices])):
 
-            merged_list[current_index_merged] = my_list[current_index_mine]
-            current_index_mine += 1
+def subset_sum1000(list1):
+    target = 1000
+    partial = []
 
-        # case: next comes from Alice's list
-        else:
-            merged_list[current_index_merged] = \
-                alices_list[current_index_alices]
-            current_index_alices += 1
+    def subset_sum(list1, target, partial=[]):
+        s = sum(partial)
+        if s >= target:
+            return  # if we reach the number why bother to continue
 
-        current_index_merged += 1
-
-    return merged_list
+    for i in range(len(list1)):
+            n = list1[i]
+            remaining = list1[i + 1:]
+            subset_sum(remaining, target, partial + [n])
--- bigOviz.py
+++ bigOviz.py
@@ -1,55 +1,68 @@
 '''
 TO-DO List
+- make the plot hold at the end for the user to analyze
 - introduce a bit of OOP to make code easier to read/update
-    - one way of doing this could be for getting the functions from the algorithms py
+    - one way of doing this could be for getting the functions from the
+    algorithms py
 - refactor code
     - come up with better variable names
     - comment certain things
-- refine argparse
-- make the program terminate and report fatal error if functions take different number of inputs
+- make the program terminate and report fatal error if functions take different
+number of inputs
 - create readme file
 - make other checks to make sure the user didn't screw up?
 - make the program more flexible:
-    - works for comparing functions that take in objects other than just lists as objects
+    - works for comparing functions that take in objects other than just lists
+    as objects
     - give option to modify the generate random list option:
         -switch from ordered to random to something else
         -provide their own inputs
- - AppleScript or simple app that takes a drag and drop algorithms.py or .txt file to trigger all the functionality
+ - AppleScript or simple app that takes a drag and drop algorithms.py or .txt
+ file to trigger all the functionality
 - Time complexity detection
     - Scipy module that finds polynomial variables
-        - Try log n, n, n log n, n^x, x^n, x! and choose the one that can find the closest root mean square
+        - Try log n, n, n log n, n^x, x^n, x! and choose the one that can find
+        the closest root mean square
     - Machine learning
         - CNN of graph image?
         - Logistic regression?
     - Anything more simple?
-- Size complexity detection 
-- Function picker that optimized for a certain n size or storage size or scalability or compromise thereof
-- Check returns of the functions to make sure they are equal. Raise error if not
-- Spit out time estimates in the verbosity section. Don’t forget to deduct the overhead of calling timeit and (especially) lambda
-    - Verify that lambda takes the same amount of time regardless of which function it takes ( in the context of this application, of course)
-- Start editing on GitHub (for backup, contribution, and portfolio reasons)
+- Size complexity detection
+- Function picker that optimized for a certain n size or storage size or
+scalability or compromise thereof
+- Check returns of the functions to make sure they are equal. Raise error if
+not
+- Spit out time estimates in the verbosity section. Don’t forget to deduct the
+overhead of calling timeit and (especially) lambda
+    - Verify that lambda takes the same amount of time regardless of which
+    function it takes ( in the context of this application, of course)
 - Incorporate unit testing. Use Pytest?
 - Try to use lambdas, list comprehensions, and decorators as much as possible
-- Use something more sophisticated than Matplotlib (like Seaborn) and maybe also make it interactive
+- Use something more sophisticated than Matplotlib (like Seaborn) and maybe
+also make it interactive
 - Use faker for other use case data generators?
-- Add a feature to calculate average time, worst-case time, and best-case time. Also maybe add a feature which iterates over a new input to find max time and min time – trying to make them reach extremes to get a full sense of the spectrum
--  Add a feature that plays around with input type of similar style such as array vs linked lists for a list type of input. This is to see how different data structures affect the time & space complexity. Could propose improvements to user
-- Either replace functionality or add option to switch to loafing a .txt or .py file that is passed as an argument through the CLI instead of importing the algorithms.py library. This probably increases flexibility for users and adaptability for future expansions (to AppleScript or web app)
+- Add a feature to calculate average time, worst-case time, and best-case time.
+Also maybe add a feature which iterates over a new input to find max time and
+min time – trying to make them reach extremes to get a full sense of the
+spectrum
+-  Add a feature that plays around with input type of similar style such as
+array vs linked lists for a list type of input. This is to see how different
+data structures affect the time & space complexity. Could propose improvements
+to user
+- Either replace functionality or add option to switch to loafing a .txt or .py
+file that is passed as an argument through the CLI instead of importing the
+algorithms.py library. This probably increases flexibility for users and
+adaptability for future expansions (to AppleScript or web app)
 - Use mypy to check function input and output types. Based off this, we can:
     - Raise exceptions if the compared functions don’t match
     - Generate specialized random inputs based on detected types
-- If type annotations not used, ask user to input some details about application
+- If type annotations not used, ask user to input some details about
+application
 - ^ actually, can’t we just use .istype?
-- Make it possible for the user to specify the file name (maybe even full path?) to be used through argparse. 
-
-
- .
- .
- .
-
+- Make it possible for the user to specify the file name (maybe even full
+path?) to be used through argparse. 
  - turn into a web app?!
 '''
-
 
 
 import random
@@ -71,41 +84,49 @@ def random_list_generator(n):
 def main():
 
     parser = argparse.ArgumentParser(description="bigOviz")
-    parser.add_argument('--repititions', nargs='?', default=5, type=int)
+    parser.add_argument('--reps',
+                        nargs='?', default=2, type=int,
+                        help="set the number of times to repeat each \
+                        algorithm for the given input size")
     parser.add_argument('--max_time_per_loop',
-                        nargs='?', default=2, type=float)
-    parser.add_argument('--n_initial', nargs='?', default=2000, type=int)
-    parser.add_argument('--n_max', nargs='?', default=50000, type=int)
-    parser.add_argument('--n_increment', nargs='?', default=500, type=int)
-    # need to verify logic and clarify!
-    parser.add_argument('-p', '--pause_for_user_input', action='store_false')
-    parser.add_argument('-v', '--verbose', help="outputs results of the join \
-                         to Terminal command line", action='store_true')
+                        nargs='?', default=2, type=float,
+                        help="set the max time the program is allowed to run \
+                        per input size increment before prompting user for \
+                        instruction to continue or quit")
+    parser.add_argument('--n_initial',
+                        nargs='?', default=1, type=int,
+                        help="set input size to start with")
+    parser.add_argument('--n_max',
+                        nargs='?', default=10, type=int,
+                        help="set input size to end with")
+    parser.add_argument('--n_increment',
+                        nargs='?', default=1, type=int,
+                        help="set input size increment")
+    # need to verify logic for this feature and clarify!
+    parser.add_argument('-p', '--pause_for_user_input',
+                        action='store_false',
+                        help="select this option to be prompted whether or \
+                        not the program should continue once increment loops \
+                        have reached the max_time_per_loop")
+    # add more info to the verbose option? function results?
+    parser.add_argument('-v', '--verbose',
+                        action='store_true',
+                        help="prints algorithm function names to the CLI")
     args = parser.parse_args()
+
+    reps = args.reps
+    max_time_per_loop = args.max_time_per_loop
+    n_initial = args.n_initial
+    n_max = args.n_max
+    n_increment = args.n_increment
+    pause_for_user_input = args.pause_for_user_input
 
     func_list = [o[0] for o in getmembers(algorithms) if isfunction(o[1])]
 
     if args.verbose:
+        print("performing analysis on the following functions:")
         for func in func_list:
-            print(func)
-
-    '''
-    DEPRECATED
-    # some default parameters:
-    repititions = 5
-    max_time_per_loop = 2  # if not specified, set default?
-    n_initial = 2000  # if not specified, default to 0?
-    n_max = 50000  # if not specified, set default?
-    n_increment = 500  # if not specified, set default?
-    pause_for_input = False
-    '''
-
-    repititions = args.repititions
-    max_time_per_loop = args.max_time_per_loop  # if not specified, set default?
-    n_initial = args.n_initial  # if not specified, default to 0?
-    n_max = args.n_max  # if not specified, set default?
-    n_increment = args.n_increment  # if not specified, set default?
-    pause_for_user_input = args.pause_for_user_input
+            print('    - {}'.format(func))
 
     testRange = range(n_initial, n_max, n_increment)
 
@@ -115,21 +136,20 @@ def main():
 
     # check to see if all functions take same number of inputs
     for func in func_list[1:]:
-        if qty_list_inputs != len(signature(getattr(algorithms, func)).parameters):
+        input_check = len(signature(getattr(algorithms, func)).parameters)
+        if input_check != qty_list_inputs:
             print("This is an error message!")
 
     # initialize plot
     plt.interactive(True)
-
     func_line_colors = []
     for i in range(len(func_list)):
         # dummy point out of range just to get the legend to work)
-        plot_line, = plt.plot(-1, -1, 'o', label=func_list[i])
+        plot_line, = plt.plot(-1, -1, '-', label=func_list[i])
         func_line_colors.append(plot_line.get_c())
-
     plt.legend()
     plt.ylabel('average execution time')
-    plt.xlabel('starting list size')
+    plt.xlabel("starting input size 'n' ")
 
     func_list_results = []
     for _ in func_list:
@@ -139,27 +159,33 @@ def main():
         loop_start_time = time.time()
         random_lists = [random_list_generator(
             n) for random_list in range(qty_list_inputs)]
+
         for i in range(len(func_list_results)):
             func_list_results[i].append(timeit.timeit(lambda: getattr(
-                algorithms, func_list[i])(*random_lists), number=repititions) / repititions)
+                algorithms, func_list[i])(*random_lists), number=reps) / reps)
         loop_end_time = time.time()
+        loop_time = loop_end_time - loop_start_time
 
         for i in range(len(func_list_results)):
-            plt.plot(testRange[:testRange.index(
-                n) + 1], func_list_results[i], "o", color=func_line_colors[i], label=func)
+            plt.plot(testRange[:testRange.index(n) + 1],
+                     func_list_results[i], color=func_line_colors[i])
 
-        plt.axis([0, max(testRange), 0, max([max(func_results)
-                                             for func_results in func_list_results])])
+        x_max = max(testRange)
+        y_max = max([max(func_results) for func_results in func_list_results])
+        plt.axis([0, x_max, 0, y_max])
         plt.show()
         plt.pause(0.05)
 
-        if loop_end_time - loop_start_time > max_time_per_loop and pause_for_user_input is False:
-            pause_for_user_input -= 1
-            print("new iterations are now taking over {}".format(max_time_per_loop),
-                  "seconds to complete. Want to continue?")
+        if loop_time > max_time_per_loop and pause_for_user_input is False:
+            pause_for_user_input -= 1  # need comment to explain this fuckery
+            time_limit_prompt = "\n" +\
+                                "New iterations are now taking over {}" +\
+                                " seconds to complete."
+            print(time_limit_prompt.format(max_time_per_loop))
             print(
-                "y: yes, continue until the end\n",
-                "n: no, quit\n",
+                "Want to continue?\n"
+                "    y: yes, continue until the end\n",
+                "   n: no, quit",
             )
 
             while True:
@@ -173,7 +199,8 @@ def main():
                 continue
             elif pause_for_user_input == "n":
                 quit()
-    # pause when everything is done so that user can still look at the plot (should find better soltion)
+
+    print("done!")
     plt.pause(1000)
 
 
